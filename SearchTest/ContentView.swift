d71877c3105ed7f1cb04646750f1ab0c9ea01a15
이경민 <cow970814@naver.com>
2022-05-12 12:45:46 +0900

    Initial Commit

---
+++ SearchTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchTest
+//
+//  Created by 이경민 on 2022/05/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchTest/SearchTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchTestApp.swift
+//  SearchTest
+//
+//  Created by 이경민 on 2022/05/12.
+//
+
+import SwiftUI
+
+@main
+struct SearchTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
